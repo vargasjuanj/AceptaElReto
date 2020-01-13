527b5d5f59d5e28d42136fbd1c398f003abdd9b7
vargasjuanj <juanjovargas443377@gmail.com>
2020-01-13 16:01:13 -0300

    Primer Test, funcion harcodeada

--- src/A103.java
+++ src/A103.java
@@ -1,16 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 //Sumas de Riemann
 
 
 public class A103 {
 
-    double recorrido = 1, rectangulos = 1000, delta = recorrido / rectangulos;//delta es el anchode cada rectangulo
-
+    double recorrido = 1, rectangulos, delta;//delta es el ancho de cada rectangulo
+int cf;
+List<Integer>param= new ArrayList();
     public static void main(String[] args) {
         new A103().calcularTerreno();
     }
 
     private void calcularTerreno() {
-        double pos = 0, alto = 0, areaCain = 0, areaAbel = 0, res = 0;
+        Scanner sc= new Scanner(System.in);
+        cf=sc.nextInt();
+        int aux=0;
+        for (int i=0; i<(cf+1); i++){
+            aux=sc.nextInt();
+            param.add(aux);
+        }
+        rectangulos=sc.nextInt();
+        delta=recorrido/rectangulos;
+        double pos = 0, alto = 0, areaCain = 0;
         for (int i = 0; i < rectangulos; i++) {
             System.out.println("pos " + pos);
             alto = calcularAlto(pos);
@@ -24,8 +39,9 @@ public class A103 {
         
     }
 
-    private double calcularAlto(double pos) {
-        return  Math.pow(pos, 3)+(2*Math.pow(pos, 2))-pos+1;
+    private double calcularAlto(double pos) { //función
+        int tam=param.size()-1;
+        return  param.get(0)*Math.pow(pos, tam)+(param.get(1)*Math.pow(pos, tam-=1))+(param.get(2)*pos)+param.get(3);
     }
     
  private void reparto(double areaCain){
