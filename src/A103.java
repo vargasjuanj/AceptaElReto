7d93fbf4b6f1fff99c7ba73da0bed4e4deb5b150
vargasjuanj <juanjovargas443377@gmail.com>
2020-01-13 22:08:39 -0300

    Se muestran todos los resultados al final de la ejecución.

--- src/A103.java
+++ src/A103.java
@@ -1,57 +1,73 @@
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 //Sumas de Riemann
-
-
 public class A103 {
 
     double recorrido = 1, rectangulos, delta;//delta es el ancho de cada rectangulo
-int cf;
-List<Integer>param= new ArrayList();
+    static int cf;
+    static Scanner sc = new Scanner(System.in);
+    static List<String> resultados = new ArrayList();
+
+    List<Integer> param = new ArrayList();
+
     public static void main(String[] args) {
-        new A103().calcularTerreno();
+
+        cf = sc.nextInt();
+
+        while (cf > -1 && cf < 20) {
+            new A103().calcularTerreno();
+            cf = sc.nextInt();
+
+        }
+        mostrar(resultados);
     }
 
     private void calcularTerreno() {
-        Scanner sc= new Scanner(System.in);
-        cf=sc.nextInt();
-        int aux=0;
-        for (int i=0; i<(cf+1); i++){
-            aux=sc.nextInt();
+
+        int aux = 0;
+        for (int i = 0; i < (cf + 1); i++) {
+            aux = sc.nextInt();
             param.add(aux);
         }
-        rectangulos=sc.nextInt();
-        delta=recorrido/rectangulos;
+        rectangulos = sc.nextInt();
+        delta = recorrido / rectangulos;
         double pos = 0, alto = 0, areaCain = 0;
         for (int i = 0; i < rectangulos; i++) {
             alto = calcularAlto(pos);
-            pos += delta ;
-            if(alto<0)continue;
-            else if(alto>1)alto=1;
-           
+            pos += delta;
+            if (alto < 0) {
+                continue;
+            } else if (alto > 1) {
+                alto = 1;
+            }
+
             areaCain += (delta * alto);
         }
         reparto(areaCain);
-        
+
     }
 
     private double calcularAlto(double pos) { //función
-        int tam=param.size()-1,i=0;
-        double alto=0;
-        for ( i=0; i<param.size()-1; i++){
-            alto+=param.get(i)*Math.pow(pos, tam);
-            tam-=1;
+        if (cf == 0) {
+            return 1;
         }
-        alto+=param.get(i);
+
+        int tam = param.size() - 1, i = 0;
+        double alto = 0;
+        for (i = 0; i < param.size() - 1; i++) {
+            alto += param.get(i) * Math.pow(pos, tam);
+            tam -= 1;
+        }
+        alto += param.get(i);
+
         return alto; //param.get(0)*Math.pow(pos, tam)+(param.get(1)*Math.pow(pos, tam-=1))+(param.get(2)*pos)+param.get(3);
     }
-    
- private void reparto(double areaCain){
-     String max="";
-double areaAbel = 1 - areaCain,res=0;
+
+    private void reparto(double areaCain) {
+        String max = "";
+        double areaAbel = 1 - areaCain, res = 0;
         if (areaCain < areaAbel) {
             max = "ABEL";
             res = areaAbel - areaCain;
@@ -65,9 +81,14 @@ double areaAbel = 1 - areaCain,res=0;
         if (res <= 0.001) {
             max = "JUSTO";
         }
-        System.out.println(max);
- 
- }
-     
-   
+        resultados.add(max);
+
+    }
+
+    private static void mostrar(List<String> resultados) {
+        for (int i = 0; i < resultados.size(); i++) {
+            System.out.println(resultados.get(i));
+        }
+    }
+    
 }
